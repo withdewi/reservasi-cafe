195b1792baff1b724040c0d354f5dd05bfb1bab0
mwilians <meitawilianisa@gmail.com>
2023-03-05 02:28:00 +0700

    push menu category

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -20,12 +20,6 @@ class CategoryController extends Controller
         return view('admin.add-categories');
     }
 
-    public function editCategory($id)
-    {
-        $category = Category::find($id);
-        return view('admin.edit-categories', compact('category'));
-    }
-
     public function storeCategory(Request $request)
     {
         // $image = $request->file('image')->store('admin/categories');
@@ -44,6 +38,34 @@ class CategoryController extends Controller
             $data->file = $request->file('file')->getClientOriginalName();
             $data->save();
         }
-        return redirect()->route('category')->with('success','Data Berhasil Di Tambahkan');
+        return redirect()->route('category');
+    }
+
+    public function editCategory($id)
+    {
+        $category = Category::find($id);
+        return view('admin.edit-categories', compact('category'));
+    }
+
+    public function updateCategory(Request $request, $id)
+    {
+        $data = Category::find($id);
+        $data->update($request->all());
+
+        if($request->hasFile('file')) {
+
+            $request->file('file')->move('kategori/', $request->file('file')->getClientOriginalName());
+            $data->file = $request->file('file')->getClientOriginalName();
+            $data->update();
+        }
+        return redirect()->route('category');
+    }
+
+    public function destroyCategory(Request $request, $id) 
+    {
+        $data = Category::find($id);
+        $data->delete();
+
+        return redirect()->route('category');
     }
 }
